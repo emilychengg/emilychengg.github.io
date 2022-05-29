a286d2384e1c92fce47e0c15d254d763ece0106e
Emily <0emilycheng@gmail.com>
2022-05-28 20:04:35 -0700

    Tinkered with navbar and fonts.

---
+++ src/pages/Experience.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Experience.css";
+
+class Experience extends Component {
+    render() {
+        return(
+            <p>Experience Page</p>
+        )
+    }
+}
+
+export default Experience; 
