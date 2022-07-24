d807b3f4c6c42d4d24d9007cf6f6190b6fac944e
Emily <0emilycheng@gmail.com>
2022-07-23 23:28:11 -0700

    Updates

---
+++ precache-manifest.8bde5b9b02d955d808ce3a53a727aef6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b480e79fa47118e70c18",
+    "url": "/static/css/main.01bac0dc.chunk.css"
+  },
+  {
+    "revision": "b480e79fa47118e70c18",
+    "url": "/static/js/main.6054e0f0.chunk.js"
+  },
+  {
+    "revision": "4607e3079765472bd05b",
+    "url": "/static/js/runtime~main.0a7edbbb.js"
+  },
+  {
+    "revision": "c38d2f1b5e801be876d5",
+    "url": "/static/js/2.f4c7524d.chunk.js"
+  },
+  {
+    "revision": "00bf9163ebc27c329c71",
+    "url": "/static/js/3.f45e74d8.chunk.js"
+  },
+  {
+    "revision": "04dc25e15b85eab800cba376e8c77fb3",
+    "url": "/static/media/emilychenglogononame.04dc25e1.png"
+  },
+  {
+    "revision": "3816e22e2987f5a139a60ce27dcdad01",
+    "url": "/static/media/emilychenglogo.3816e22e.png"
+  },
+  {
+    "revision": "5bae93eb223e51b860fd5c1f008186c2",
+    "url": "/static/media/aboutphoto.5bae93eb.JPG"
+  },
+  {
+    "revision": "647222c12287d4bf0b1c789a34c06144",
+    "url": "/index.html"
+  }
+];
